dc1a57ccd4bf34e7e8be527d2f6bc49c1faba963
CaityHeath <caity.heath@gmail.com>
2019-08-26 20:54:20 -0700

    file-scaffolding

---
+++ src/footer.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+
+class Footer extends Component {
+  state = {
+
+  }
+
+  render(){
+    return(
+   
+      <div className="footer-content">
+        <div className="hours">
+      
+        Monday - Sunday 11am - 7pm
+        </div>
+
+        <div className="location">
+          302 4th Ave E, Olympia, WA 98501
+        </div>
+
+        <div className="phone">
+          360 405-3772
+        </div>
+      </div>
+
+    )
+  }
+}
+
+export default Footer;
